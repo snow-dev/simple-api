fed3f9861d1fbe11199c533c24f4c268b38cdce4
Snow Dev <carlosavila@appeal.com.mx>
2019-11-11 00:55:22 -0600

    SA-003 0. We enable correct systems interruptions for shutdown server properly. 1. We define base blog actions, create, read and delete a blog. 2. We define the internal logic for above created methods.

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/gorilla/mux"
 	"log"
+
 	"net/http"
 )
 
@@ -22,19 +19,19 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", homeLink)
-	//log.Fatal(http.ListenAndServe(":8000", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8000", router))
 
 	//Set up a context required by mongo.Connect
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) //To close the connection at the end
-	defer cancel()                                                           //We need to set up a client first
-	//It takes the URI of your database
-	client, error := mongo.NewClient(options.Client().ApplyURI("your_database_uri"))
-	if error != nil {
-		log.Fatal(error)
-	} //Call the connect function of client
-	error = client.Connect(ctx) //Checking the connection
-	error = client.Ping(context.TODO(), nil)
-	fmt.Println("Database connected")
+	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) //To close the connection at the end
+	//defer cancel()                                                           //We need to set up a client first
+	////It takes the URI of your database
+	//client, error := mongo.NewClient(options.Client().ApplyURI("your_database_uri"))
+	//if error != nil {
+	//	log.Fatal(error)
+	//} //Call the connect function of client
+	//error = client.Connect(ctx) //Checking the connection
+	//error = client.Ping(context.TODO(), nil)
+	//fmt.Println("Database connected")
 }
--- server/main.go
+++ server/main.go
@@ -1,33 +1,196 @@
 package main
 
 import (
-	blogpb "../proto"
+	blogpb "github.com/snow-dev/simple-api/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+	"os"
+	"os/signal"
 
 	"fmt"
 	"log"
 	"net"
 )
 
-type BlogServiceServer struct {}
+type BlogServiceServer struct{}
+
+func (s BlogServiceServer) CreateBlog(ctx context.Context, req *blogpb.CreateBlogReq) (*blogpb.CreateBlogRes, error) {
+	// Essentially doing req.Blog to access the struct with a nil check
+	blog := req.GetBlog()
+	// Now we have to convert it into a BlogItem type to convert into BSON
+	data := BlogItem{
+		//ID:		empty so it gets omitted and MongoDB generates a unique Object ID upont insertion
+		AuthorID: blog.GetAuthorId(),
+		Content:  blog.GetContent(),
+		Title:    blog.GetTitle(),
+	}
+
+	// Insert the data into the database, result contain the newly generated Object ID for de new document.
+	result, err := blogdb.InsertOne(mongoCtx, data)
+	// Check for potential errors.
+	if err != nil {
+		// return internal gRPC error to be handled later.
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error: %v", err),
+		)
+	}
+
+	// Add the id to the blog, first cast the "generic type" (go doesn't have real generics yet)
+	oid := result.InsertedID.(primitive.ObjectID)
+	// Convert the object id to it's string counterpart.
+	blog.Id = oid.Hex()
+	// Return the blog in a CreateBlogRes type.
+	return &blogpb.CreateBlogRes{Blog: blog}, nil
+}
+
+func (s BlogServiceServer) ReadBlog(ctx context.Context, req *blogpb.ReadBlogReq) (*blogpb.ReadBlogRes, error) {
+	// convert string id (from proto) to MongoDB ObjectId
+	oid, err := primitive.ObjectIDFromHex(req.GetId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Could not convert to ObjectId: %v", err))
+	}
+	result := blogdb.FindOne(ctx, bson.M{"_id": oid})
+	// Create an empty BlogItem to write our decode result to
+	data := BlogItem{}
+	// decode and write to data
+	if err := result.Decode(&data); err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Could not find blog with Object Id %s : %v", req.GetId(), err))
+	}
+	//cast to reaadBlogres type
+	response := &blogpb.ReadBlogRes{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: data.AuthorID,
+			Title:    data.Title,
+			Content:  data.Content,
+		},
+	}
+	return response, nil
+}
+
+func (b BlogServiceServer) UpdateBlog(context.Context, *blogpb.UpdateBlogReq) (*blogpb.UpdateBlogRes, error) {
+	panic("implement me")
+}
+
+func (s BlogServiceServer) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogReq) (*blogpb.DeleteBlogRes, error) {
+	// get the ID (string) from the request message and convert it to an abject ID
+	oid, err := primitive.ObjectIDFromHex(req.GetId())
+	// Check errors
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Could not convert to ObjectId: %v", err))
+	}
+	// DeleteOne returns DeleteResult which is a struct containing the amount of deleted docs (in this case only 1 always)
+	// So we return a boolean instead
+	_, err = blogdb.DeleteOne(ctx, bson.M{"_id": oid})
+	// Check errors.
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Could not found  find/delete blog with id %s: %v", req.GetId(), err))
+	}
+	// Return response with success: true if no errors is thrown (and this document is removed)
+	return &blogpb.DeleteBlogRes{
+		Success: true,
+	}, nil
+}
+
+func (b BlogServiceServer) ListBlogs(*blogpb.ListBlogsReq, blogpb.BlogService_ListBlogsServer) error {
+	panic("implement me")
+}
+
+var db *mongo.Client
+var blogdb *mongo.Collection
+var mongoCtx context.Context
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson"title"`
+}
 
 func main() {
 	// Configure 'log' package to give file name and line number on eg. log.Fatal
-	// Pipe flags to another (log.LtsdFlags = log.Ldate | log.Ltime)
+	// just the filename & line number:
+	// log.SetFlags(log.Lshortfile)
+	// Or add timestamps and pipe file name and line number to it:
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	fmt.Println("Starting server on port :50051...")
 
-	// Start our listener, 50051 is the default gRPC port
+	// 50051 is the default port for gRPC
+	// Ideally we'd use 0.0.0.0 instead of localhost as well
 	listener, err := net.Listen("tcp", ":50051")
-	// Handle errors if any.
+
 	if err != nil {
-		log.Fatal("Unable tolisten on port :50051: %v", err)
+		log.Fatalf("Unable to listen on port :50051: %v", err)
 	}
 
-	// Set options, here we can configure tings like TLS support
-	//opts := []grpc.ServerOption{}
-	// Create new gRPC server wth (blank) options
-	//s := grpc.NewServer(opts...)
+	// slice of gRPC options
+	// Here we can configure things like TLS
+	opts := []grpc.ServerOption{}
+	// var s *grpc.Server
+	s := grpc.NewServer(opts...)
+	// var srv *BlogServiceServer
+	srv := &BlogServiceServer{}
 
+	blogpb.RegisterBlogServiceServer(s, srv)
 
-	
+	// Initialize MongoDb client
+	fmt.Println("Connecting to MongoDB...")
+	mongoCtx = context.Background()
+	db, err = mongo.Connect(mongoCtx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping(mongoCtx, nil)
+	if err != nil {
+		log.Fatalf("Could not connect to MongoDB: %v\n", err)
+	} else {
+		fmt.Println("Connected to Mongodb")
+	}
+
+	blogdb = db.Database("mydb").Collection("blog")
+
+	// Start the server in a child routine
+	go func() {
+		if err := s.Serve(listener); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+	fmt.Println("Server succesfully started on port :50051")
+
+	// Bad way to stop the server
+	// if err := s.Serve(listener); err != nil {
+	// 	log.Fatalf("Failed to serve: %v", err)
+	// }
+	// Right way to stop the server using a SHUTDOWN HOOK
+
+	// Create a channel to receive OS signals
+	c := make(chan os.Signal)
+
+	// Relay os.Interrupt to our channel (os.Interrupt = CTRL+C)
+	// Ignore other incoming signals
+	signal.Notify(c, os.Interrupt)
+
+	// Block main routine until a signal is received
+	// As long as user doesn't press CTRL+C a message is not passed
+	// And our main routine keeps running
+	// If the main routine were to shutdown so would the child routine that is Serving the server
+	<-c
+
+	// After receiving CTRL+C Properly stop the server
+	fmt.Println("\nStopping the server...")
+	s.Stop()
+	listener.Close()
+	fmt.Println("Closing MongoDB connection")
+	db.Disconnect(mongoCtx)
+	fmt.Println("Done.")
+
 }
